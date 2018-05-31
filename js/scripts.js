09b89e84fc7ec61aab5f9428471fe1eb570b8208
Eric Shull and Kevin Ahn <kevin@>
2018-05-31 14:10:40 -0700

    add two forms for choosing which animal you like and all the jquery to branch into the information

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,57 @@
-// $(document).ready(function(){
-//   var age = parseInt(prompt("How old are you?"));
-//
-//   if (age > 21) {
-//     $('#drinks').show();
-//   } else if (age === 21) {
-//     alert("Now don't get crazy");
-//     $('#drinks').show();
-//   } else {
-//     $("#under-21").show();
-//   }
-// });
-
 $(function(){
-  var age = parseInt(prompt("What is your age?"));
+  $("#checkAge").submit(function(event) {
+    event.preventDefault();
+    var age = parseInt(prompt("What is your age?"));
+    if (age >= 18) {
+      $("#canVote").show();
+    } else {
+      $("#cannotVote").show();
+    }
+  })
 
-  if (age >= 18) {
-    $("#canVote").show();
-  } else {
-    $("#cannotVote").show();
-  }
+  $("#checkHeight").submit(function(event) {
+    event.preventDefault();
+    var height = parseInt($("input:radio[name=tallness]:checked").val());
+    $(".rideable").show();
+    $(".smallRides").hide();
+    $(".mediumRides").hide();
+    $(".tallRides").hide();
+
+    if (height === 1) {
+      $(".smallRides").show();
+    }
+    if (height === 2) {
+      $(".smallRides").show();
+      $(".mediumRides").show();
+    }
+    if (height === 3) {
+      $(".mediumRides").show();
+      $(".tallRides").show();
+    }
+  });
+
+  $(".warmOrCold").submit(function(event) {
+    event.preventDefault();
+    var firstAnswer = $("#bloodType").val();
+
+    if (firstAnswer === "warmbBlood") {
+      $(".warmOrCold").hide();
+      $(".mammals").show();
+    } else {
+      $(".warmOrCold").hide();
+      $(".scalyOrSlimy").show();
+    }
+  })
+
+  $(".scalyOrSlimy").submit(function(event) {
+    event.preventDefault();
+    var secondAnswer = $("#skinType").val();
+    if (secondAnswer === "scaly") {
+      $(".scalyOrSlimy").hide();      $(".reptile").show();
+    } else {
+      $(".scalyOrSlimy").hide();
+      $(".amphibian").show();
+    }
+  });
+
 });
